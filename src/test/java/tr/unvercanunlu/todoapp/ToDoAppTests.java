ada963fb84cee04abed673a0ab76d1dbccce44c4
unver <unvercanunlu@gmail.com>
2025-06-27 10:19:24 +0200

    initial commit

---
+++ src/test/java/tr/unvercanunlu/todoapp/ToDoAppTests.java
@@ -0,0 +1,13 @@
+package tr.unvercanunlu.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
